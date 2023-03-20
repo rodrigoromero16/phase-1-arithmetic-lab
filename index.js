83dd33c7fa519c7d4a8460dd4b167e5d86bf82df
Rodrigo <rromero22398@gmail.com>
2023-03-19 21:14:07 -0400

    Arithmetic Lab

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+
+let num1 = 1;
+let num2 = 62;
+const num3 = 14;
+const num4 = 5;
+
+let multiply = num1 * num2;
+
+let mod = num3 % num4;
+
+let max = Math.max(5,10,15,20);
+
+let random = Math.floor(Math.random() * 10)+1;
+Math.isInteger(random);
